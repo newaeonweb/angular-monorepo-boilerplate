5f7c1ec4e58d97ee51c6fc3a5cd78f414dc4f305
newaeonweb <fernando@newaeonweb.com.br>
2019-05-31 13:21:00 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mab-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'monorepo-angular-boilerplate';
+}
