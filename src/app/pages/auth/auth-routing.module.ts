9880ea07a3cc6cc24a9e57db3478cd395e075bd7
newaeonweb <fernando@newaeonweb.com.br>
2019-06-02 13:20:30 -0300

    added notfound to auth routes

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 // Only Auth related components here
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from 'src/app/core/components/not-found.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
