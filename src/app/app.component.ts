73d5b6f77b182bd41d5ddcffa3fc77cb72978d63
newaeonweb <fernando@newaeonweb.com.br>
2019-06-04 15:43:05 -0300

    simplify home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './auth/_model/user';
+import { Store, select } from '@ngrx/store';
+import { State } from './statemanagement';
+import { LogOut } from './statemanagement/auth.actions';
 
 @Component({
   selector: 'mab-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'monorepo-angular-boilerplate';
+
+  getState: Observable<any>;
+  isAuthenticated = false;
+  user: User = null;
+  errorMessage = null;
+
+  constructor(private store: Store<State>) {
+    this.getState = store.pipe(select('auths'));
+  }
+
+  ngOnInit() {
+    this.getState.subscribe((state) => {
+      console.log(state);
+      this.isAuthenticated = state.isAuthenticated;
+      this.user = state.user;
+      this.errorMessage = state.errorMessage;
+    });
+  }
+
+  logout(): void {
+    this.store.dispatch(new LogOut());
+  }
+
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,67 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-// NGRX State management
-import { Store, select } from '@ngrx/store';
-import { State } from 'src/app/statemanagement';
-import { LogOut } from 'src/app/statemanagement/auth.actions';
-
-// App models
-import { User } from '../../auth/_model/user';
 
 @Component({
   selector: 'mab-app-home',
   template: `
-  <div class="row">
-  <div class="col-md-12 text-center">
-    <hr><br>
-    <div *ngIf="isAuthenticated; then doSomething; else doSomethingElse;"></div>
-    <ng-template #doSomething>
-      <p>You logged in <em>{{user.email}}!</em></p>
-      <button class="btn btn-primary" (click)="logout()">Log out</button>
-    </ng-template>
-    <ng-template #doSomethingElse>
-      <p>You are not logged</p>
-      <a [routerLink]="['/login']" class="btn btn-primary">Login</a>
-    </ng-template>
-    <div>
-    <br>
-      <h5 class="card-title">Current State</h5>
-      <ul class="list-unstyled">
-        <li><strong>isAuthenticated</strong> - {{isAuthenticated}}</li>
-        <li><strong>user.email</strong> - {{ user?.email || 'null'}}</li>
-        <li><strong>user.token</strong> - {{ user?.token || 'null'}}</li>
-        <li><strong>errorMessage</strong> - {{ errorMessage || 'null'}}</li>
-      </ul>
-    </div>
-
-  </div>
-</div>
+  <p>
+    Core Application, home route works!
+  </p>
   `,
   styles: []
 })
 export class HomeComponent implements OnInit {
 
-  getState: Observable<any>;
-  isAuthenticated = false;
-  user: User = null;
-  errorMessage = null;
-
-  constructor(private store: Store<State>) {
-    this.getState = store.pipe(select('auths'));
-  }
+  constructor() {}
 
   ngOnInit() {
-    this.getState.subscribe((state) => {
-      console.log(state);
-      this.isAuthenticated = state.isAuthenticated;
-      this.user = state.user;
-      this.errorMessage = state.errorMessage;
-    });
-  }
 
-  logout(): void {
-    console.log('exit');
-    this.store.dispatch(new LogOut());
   }
 
 }
