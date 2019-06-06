22b3b2725a107396cd1f81dafc9c0335d1392ad1
newaeonweb <fernando@newaeonweb.com.br>
2019-06-06 11:05:41 -0300

    removed extra methods

--- src/app/statemanagement/auth.reducer.ts
+++ src/app/statemanagement/auth.reducer.ts
@@ -44,55 +44,9 @@ export function authReducer( state = initialState, action: AuthActions ): AuthSt
       return initialState;
     }
 
-    case AuthActionTypes.AddAuth: {
-      return adapter.addOne(action.payload.auth, state);
-    }
-
-    case AuthActionTypes.UpsertAuth: {
-      return adapter.upsertOne(action.payload.auth, state);
-    }
-
-    case AuthActionTypes.AddAuths: {
-      return adapter.addMany(action.payload.auths, state);
-    }
-
-    case AuthActionTypes.UpsertAuths: {
-      return adapter.upsertMany(action.payload.auths, state);
-    }
-
-    case AuthActionTypes.UpdateAuth: {
-      return adapter.updateOne(action.payload.auth, state);
-    }
-
-    case AuthActionTypes.UpdateAuths: {
-      return adapter.updateMany(action.payload.auths, state);
-    }
-
-    case AuthActionTypes.DeleteAuth: {
-      return adapter.removeOne(action.payload.id, state);
-    }
-
-    case AuthActionTypes.DeleteAuths: {
-      return adapter.removeMany(action.payload.ids, state);
-    }
-
-    case AuthActionTypes.LoadAuths: {
-      return adapter.addAll(action.payload.auths, state);
-    }
-
-    case AuthActionTypes.ClearAuths: {
-      return adapter.removeAll(state);
-    }
-
     default: {
       return state;
     }
   }
 }
 
-export const {
-  selectIds,
-  selectEntities,
-  selectAll,
-  selectTotal,
-} = adapter.getSelectors();
