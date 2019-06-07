d7190b1067b3aac5e3b32906ed07756d558641de
newaeonweb <fernando@newaeonweb.com.br>
2019-06-07 12:24:17 -0300

    added new services

--- src/app/auth/_service/auth.service.ts
+++ src/app/auth/_service/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_model/user';
 import { environment } from 'src/environments/environment';
@@ -15,6 +15,39 @@ export class AuthService {
 
   getToken(): string {
     return localStorage.getItem('token');
+  }
+
+  setToken(token: string): void {
+    return localStorage.setItem('token', token );
+  }
+
+  removeToken(): void {
+    localStorage.removeItem('token');
+  }
+
+  isAuthenticated(): boolean {
+    // get the token
+    const token: string = this.getToken();
+    if (token) {
+      return true;
+    }
+    return false;
+  }
+
+  checkToken() {
+    const token = this.getToken();
+    if (token) {
+      console.log('yes')
+      const httpOptions = {
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        })
+      };
+      const url = `${this.API_URL}/status`;
+      return this.http.get(url, httpOptions);
+    }
+
   }
 
   login(email: string, password: string): Observable<any> {
