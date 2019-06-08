9cc54cfe61f9486424207b1d0d72df929d15b036
newaeonweb <fernando@newaeonweb.com.br>
2019-06-08 11:45:47 -0300

    simplified states

--- src/app/auth/_statemanagement/auth.model.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../_model/user';
-
-export interface Auth {
-  isAuthenticated: boolean;
-  user: User | null;
-  errorMessage: string | null;
-}
--- src/app/auth/_statemanagement/auth.reducer.ts
+++ src/app/auth/_statemanagement/auth.reducer.ts
@@ -1,23 +1,17 @@
-import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
-import { Auth } from './auth.model';
 import { AuthActions, AuthActionTypes } from './auth.actions';
 import { User } from '../_model/user';
 
-export interface AuthState extends EntityState<Auth> {
-  // additional entities state properties
+export interface AuthState {
   isAuthenticated: boolean;
   user: User | null;
   errorMessage: string | null;
 }
 
-export const adapter: EntityAdapter<Auth> = createEntityAdapter<Auth>();
-
-export const initialState: AuthState = adapter.getInitialState({
-  // additional entity state properties
+export const initialState: AuthState = {
   isAuthenticated: false,
   user: null,
   errorMessage: null
-});
+};
 
 export function authReducer( state = initialState, action: AuthActions ): AuthState {
   switch (action.type) {
