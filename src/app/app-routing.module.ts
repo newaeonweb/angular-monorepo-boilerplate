bdc7b43a0d79c08b2091cac525257f774b1b1b26
newaeonweb <fernando@newaeonweb.com.br>
2019-08-05 20:30:43 -0300

    removed uneeded import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,34 +6,9 @@ import { AdminSharedModule } from 'projects/admin/src/app/app.module';
 import { DashboardSharedModule } from 'projects/dashboard/src/app/app.module';
 
 // App components
-import { NotFoundComponent } from './core/components/not-found.component';
 import { HomeModule } from './pages/home/home.module';
 
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./pages/home/home.module').then(m => m.HomeModule),
-  },
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('../../projects/admin/src/app/app.module').then(
-        m => m.AdminSharedModule
-      ),
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () =>
-      import('../../projects/dashboard/src/app/app.module').then(
-        m => m.DashboardSharedModule
-      ),
-  },
-  // {
-  //   path: '**',
-  //   component: NotFoundComponent
-  // }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [
