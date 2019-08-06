1db140cdb414fbb2f4dfd3dc56e3830848397d24
newaeonweb <fernando@newaeonweb.com.br>
2019-08-05 23:05:10 -0300

    update guards

--- projects/admin/src/app/pages/home/home-routing.module.ts
+++ projects/admin/src/app/pages/home/home-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { AuthGuard } from 'src/app/auth/_service/auth.guard';
 
 const routes: Routes = [
   {
     path: 'admin',
     component: HomeComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: '',
--- projects/dashboard/src/app/pages/home/home-routing.module.ts
+++ projects/dashboard/src/app/pages/home/home-routing.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { AuthGuard } from 'src/app/auth/_service/auth.guard';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: HomeComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: '',
     redirectTo: 'dashboard',
-    pathMatch: 'full'
-  }
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
